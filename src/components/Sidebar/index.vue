65d844b9538559828314ea42b383a2cff15c65fc
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2020-08-22 16:35:55 -0300

    feat: sidebar component

--- src/components/Sidebar/index.vue
+++ src/components/Sidebar/index.vue
@@ -1,13 +1,101 @@
 <template>
   <div class="sidebar">
-    <p>sidebar</p>
+    <div class="sidebar__profile">
+      <user-info
+        image="profile.jpg"
+        username="clodoaldodantas8"
+        display_name="Clodoaldo Dantas"
+      />
+    </div>
+    <div class="suggestions">
+      <div class="suggestions__header">
+        <h3>Sugestões para você</h3>
+        <a href="#">ver tudo</a>
+      </div>
+      <ul class="suggestions__list">
+        <li v-for="(user, index) in users" :key="index" class="suggestion">
+          <user-info
+            :image="user.image"
+            :username="user.username"
+            :display_name="user.display_name"
+            :small="true"
+          />
+          <a class="primary" href="#">seguir</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import UserInfo from '@/components/UserInfo';
+
 export default {
-  name: 'sidebar'
+  name: 'sidebar',
+  components: {
+    UserInfo
+  },
+  data() {
+    return {
+      users: [
+        {
+          username: 'traversymedia',
+          display_name: 'Traversy Media',
+          image: 'traversy.jpg'
+        },
+        {
+          username: 'rocketseat',
+          display_name: 'Rocketseat',
+          image: 'rocketseat.jpg'
+        },
+        {
+          username: 'filipedeschamps',
+          display_name: 'Filipe Deschamps',
+          image: 'filipe.jpg'
+        }
+      ]
+    };
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.sidebar {
+  &__profile {
+    margin-top: 1.8rem;
+  }
+}
+
+.suggestions {
+  &__header {
+    padding: 1.6rem 0 0.8rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    h3 {
+      font-weight: bold;
+      font-size: 1.4rem;
+      color: var(--color-medium);
+    }
+
+    a {
+      font-size: 1.4rem;
+      color: var(--color-regular);
+      font-weight: bold;
+    }
+  }
+}
+
+.suggestion {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.primary {
+  font-size: 1.4rem;
+  font-weight: bold;
+  color: var(--color-primary);
+}
+</style>
---
+++ src/components/UserInfo/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="user-info">
+    <img
+      class="user-info__image"
+      :class="{ small: small }"
+      :src="resolveImage"
+      alt="Perfil do usuário"
+    />
+    <div class="user-info__content">
+      <strong>{{ username }}</strong>
+      <span>{{ display_name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'user-info',
+  props: {
+    username: String,
+    display_name: String,
+    image: String,
+    small: Boolean
+  },
+  computed: {
+    resolveImage() {
+      return require(`@/assets/${this.image}`);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.user-info {
+  display: flex;
+  align-items: center;
+  margin: 0.8rem 0;
+
+  &__image {
+    height: 5.6rem;
+    width: 5.6rem;
+    border-radius: 50%;
+    margin-right: var(--gutter);
+
+    &.small {
+      height: 3.2rem;
+      width: 3.2rem;
+    }
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+
+    span {
+      font-size: 1.2rem;
+      color: var(--color-medium);
+    }
+  }
+}
+</style>
